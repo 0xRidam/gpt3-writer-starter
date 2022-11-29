ff4218aae3794ab7fead415a77de7ccfe1e336db
Ridam <officiallyridam@gmail.com>
2022-11-29 15:58:59 +0530

    committed

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  
+  const [userInput, setUserInput] = useState('')
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  
+
+  const onUserChangedtext = (event) => {
+    setUserInput(event.target.value)
+  }
+
   return (
     <div className="root">
       <Head>
@@ -11,12 +23,29 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Technical blog post about Ethereum</h1>
           </div>
           <div className="header-subtitle">
             <h2>insert your subtitle here</h2>
           </div>
         </div>
+        <div className='prompt-container'>
+          <textarea 
+            placeholder='Start typing here'
+            className='prompt-box'
+            value={userInput}
+            onChange={onUserChangedtext} />
+
+            <div className='prompt-buttons'>
+              <a className='generate-button' onClick={null}>
+                <div className='generate'>
+                  <p>Generate</p>
+                </div>
+              </a>
+            </div> 
+        </div>
+
+        
       </div>
       <div className="badge-container grow">
         <a
