a2b6f5df3daac97795495b7adafc958b1130d75f
Ridam <officiallyridam@gmail.com>
2022-12-05 13:49:06 +0530

    not much

--- pages/index.js
+++ pages/index.js
@@ -57,6 +57,21 @@ const Home = () => {
               </div>
             </a>
           </div>
+
+          {/*====api output====*/}
+          {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
+        
         </div>
       </div>
       <div className="badge-container grow">
